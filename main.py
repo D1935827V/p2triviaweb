77ef5c349320d10aab3f38f7254b0ef40c97f8ba
D1935827V <dvestal2102@gmail.com>
2020-10-20 10:07:35 -0700

    POG

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.route('/sub')
+def sub():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port='3000')
