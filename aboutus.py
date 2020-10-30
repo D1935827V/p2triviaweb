b6c6e1b6343d8a8fd4298f0ccb579df2e03164a9
idaa355 <idaa355@users.noreply.github.com>
2020-10-30 11:09:58 -0700

    Added About Us page and fixed link to trivia

---
+++ aboutus.py
@@ -0,0 +1,48 @@
+def nivu():
+    name = "Nivu Rethnakar"
+    birthday = "July 29"
+    mbti = "INTP"
+    funfact = "I love amber scented candles!"
+    sign = "Leo"
+    profile = {"name": name, "birthday": birthday, "mbti": mbti, "funfact": funfact, "sign": sign}
+    return profile
+
+def ida():
+    name = "Ida Mobini"
+    birthday = "April 7"
+    mbti = "ISTP"
+    funfact = "I write poetry and short stories in my free time."
+    sign = "Aries"
+    profile = {"name": name, "birthday": birthday, "mbti": mbti, "funfact": funfact, "sign": sign}
+    return profile
+
+def crystal():
+    name = "Crystal Widjaja"
+    birthday = "January 7"
+    mbti = "ENFP"
+    funfact = "I can play four instruments - piano, guitar, ukulele, and violin."
+    sign = "Capricorn"
+    profile = {"name": name, "birthday": birthday, "mbti": mbti, "funfact": funfact, "sign": sign}
+    return profile
+
+def eva():
+    name = "Eva Gravin"
+    birthday = "December 14"
+    mbti = "ESFJ"
+    funfact = "I am a former cross country and track star."
+    sign = "Sagittarius"
+    profile = {"name": name, "birthday": birthday, "mbti": mbti, "funfact": funfact, "sign": sign}
+    return profile
+
+def dane():
+    name = "Dane Vestal"
+    birthday = "July 15"
+    mbti = "ESTP"
+    funfact = "I'm a boy scout!"
+    sign = "Cancer"
+    profile = {"name": name, "birthday": birthday, "mbti": mbti, "funfact": funfact, "sign": sign}
+    return profile
+
+def about():
+    return [nivu(), ida(), crystal(), eva(), dane()]
+
